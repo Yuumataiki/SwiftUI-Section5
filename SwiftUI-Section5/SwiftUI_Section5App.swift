d75e65afa8049937bf63551d36874582c91d839b
Yuumataiki <84608371+Yuumataiki@users.noreply.github.com>
2021-06-11 18:39:17 +0900

    Initial Commit

---
+++ SwiftUI-Section5/SwiftUI_Section5App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Section5App.swift
+//  SwiftUI-Section5
+//
+//  Created by yumataiki on 2021/06/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Section5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
